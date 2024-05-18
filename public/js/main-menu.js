9ef52b4a09f0dee7930bb1307225e3145574b9f2
AustinW10 <austinwells100@yahoo.com>
2024-05-18 16:52:05 -0400

    added in rough draft of main menu layout

--- public/js/main-menu.js
+++ public/js/main-menu.js
@@ -6,10 +6,13 @@ state.player = Character.load();
 // TODO: Start creating the main menu html/css
 let combatClass = state.player.combatClass
 let race = state.player.race
+let nameElement = document.getElementById("characterName");
+
 
 const c = combatClass.toLowerCase()
 const r = race.toLowerCase()
 const imageName = `${r}-${c}.jpg`
 
+ 
 const characterImageElement = document.getElementById("characterImage");
 characterImageElement.src = `/images/characterimages/${imageName}`;
